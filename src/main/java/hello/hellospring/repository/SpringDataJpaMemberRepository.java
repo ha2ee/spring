6b766b45acdc211727db0e0c9498a721119bcaa8
KimDongHyun <donghyun.develop@gmail.com>
2023-04-28 22:17:15 +0900

    2023.04.28 SpringDataJPA

--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -8,6 +8,7 @@ import hello.hellospring.domain.Member;
 
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository{
     
+    //JPQL select m from Member m where m.name = ?
     @Override
     Optional<Member> findByName(String name);
 }
