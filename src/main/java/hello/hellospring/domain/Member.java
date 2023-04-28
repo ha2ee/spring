24c06ce4813036bb4b586f43619ae3bbfafe7ee3
KimDongHyun <donghyun.develop@gmail.com>
2023-04-28 22:08:22 +0900

    2023.04.28 SpringDataJPA

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,16 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
     
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 고객이 정하는 아이디가 아닌 시퀀스가 오르던 하는 ,임의의 값(시스템에 저장하는 아이디)
+    
     private String name;
 
     public Long getId() {
