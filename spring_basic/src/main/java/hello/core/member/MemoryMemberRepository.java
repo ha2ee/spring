0a0ad8d1d957ba70e43b8846ceb992dd0f9c76bc
KimDongHyun <donghyun.develop@gmail.com>
2023-05-05 19:41:08 +0900

    2023.05.05 스프링 기본_회원 도메인 개발

---
+++ spring_basic/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>(); //실무에선 동시성 문제로 인해 ConcurrnetHashMap을 사용해야함. 
+
+    @Override
+    public Member findById(Long memberId) {
+       return store.get(memberId);
+    }
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId() , member);
+    }
+    
+}
