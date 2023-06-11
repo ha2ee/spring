ae9a4e01b8b54cf50a07ebf209d00a95e5c7bce9
KimDongHyun <donghyun.develop@gmail.com>
2023-06-11 22:35:00 +0900

    2023.06.11 스프링 MVC1 - 상품 등록 처리 - @ModelAttribute

--- spring_mvc/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ spring_mvc/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,9 +7,11 @@ import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
@@ -41,9 +43,66 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+    // @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+            @RequestParam int price,
+            @RequestParam Integer quantity,
+            Model model) {
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute("item") Item item
+     * model.addAttribute("item", item); 자동 추가
+     */
+    // @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+
+        itemRepository.save(item);
+        // model.addAttribute("item", item); //자동 추가, 생략 가능
+
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute name 생략 가능
+     *                 model.addAttribute(item); 자동 추가, 생략 가능
+     *                 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
+     */
+    // @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item, Model model) {
+
+        // HelloData -> helloData
+        // Item -> item
+        itemRepository.save(item);
+        // model.addAttribute("item", item); //자동 추가, 생략 가능
+
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute 자체 생략 가능
+     *                 model.addAttribute(item) 자동 추가
+     */
     @PostMapping("/add")
-    public String save() {
-        return "xxx";
+    public String addItemV4(Item item) {
+
+        // HelloData -> helloData
+        // Item -> item
+        itemRepository.save(item);
+        // model.addAttribute("item", item); //자동 추가, 생략 가능
+
+        return "basic/item";
     }
 
     /**
