b6c3e66f31242e4dde638bd8d50b04e303c2893a
KimDongHyun <donghyun.develop@gmail.com>
2023-09-25 20:11:22 +0900

    2023.09.25 스프링 DB1 - 스프링 예외 추상화 적용

--- spring_db1/jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ spring_db1/jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -4,6 +4,7 @@ import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
 import hello.jdbc.repository.MemberRepository3;
 import hello.jdbc.repository.MemberRepository4_1;
+import hello.jdbc.repository.MemberRepository4_2;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
@@ -51,7 +52,8 @@ class MemberServiceV4Test {
 
         @Bean
         MemberRepository memberRepository() {
-            return new MemberRepository4_1(dataSource);
+//            return new MemberRepository4_1(dataSource);
+            return new MemberRepository4_2(dataSource);
         }
 
         @Bean
