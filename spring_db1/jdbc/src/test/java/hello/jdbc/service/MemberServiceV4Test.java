f59f2fecbe6d146edb2894e486c527f9bc320708
KimDongHyun <donghyun.develop@gmail.com>
2023-09-25 20:27:02 +0900

    2023.09.25 스프링 DB1 - JDBC 반복 문제 해결 - JdbcTemplate

--- spring_db1/jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ spring_db1/jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -1,10 +1,7 @@
 package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
-import hello.jdbc.repository.MemberRepository;
-import hello.jdbc.repository.MemberRepository3;
-import hello.jdbc.repository.MemberRepository4_1;
-import hello.jdbc.repository.MemberRepository4_2;
+import hello.jdbc.repository.*;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
@@ -53,7 +50,8 @@ class MemberServiceV4Test {
         @Bean
         MemberRepository memberRepository() {
 //            return new MemberRepository4_1(dataSource);
-            return new MemberRepository4_2(dataSource);
+//            return new MemberRepository4_2(dataSource);
+            return new MemberRepository5(dataSource);
         }
 
         @Bean
